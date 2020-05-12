419b32205b7d83247b4ea57b0beb18c3639db25c
oktavian1 <ilham.oktavian1313@gmail.com>
2020-05-13 01:02:02 +0700

    part 4 membuat data dalam activity tidak hilang saat mode landscape

--- app/src/main/java/com/example/firstkotlinandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinandroidapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firstkotlinandroidapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,18 +15,69 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var countDownTimer: CountDownTimer
     internal val initialCountDown: Long = 60000
     internal val countDownInterval: Long = 1000
+    internal var timeLeftOnTimer: Long = 60000
+
+    companion object{
+        private val TAG = MainActivity::class.java.simpleName
+
+        private const val SCORE_KEY = "score_key"
+        private const val TIME_LEFT_KEY = "time_left_key"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d(TAG, "onCreate called. score is : $score")
+
         tapMeButton.setOnClickListener{
             incrementScore()
         }
 
+        if (savedInstanceState != null){
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeftOnTimer = savedInstanceState.getLong(TIME_LEFT_KEY)
+            restoreGame()
+        }else{
+            resetGame()
+        }
+
+
+    }
+
+    private fun restoreGame(){
         gameScoreTextView.text = getString(R.string.your_score, score)
 
-        resetGame()
+        val restoredTime = timeLeftOnTimer / 1000
+        timeLeftTextView.text = getString(R.string.time_left, restoredTime)
+
+        countDownTimer = object : CountDownTimer(timeLeftOnTimer, countDownInterval){
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftOnTimer = millisUntilFinished
+                val timeLeft = millisUntilFinished / 1000
+                timeLeftTextView.text = getString(R.string.time_left, timeLeft)
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+        }
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putLong(TIME_LEFT_KEY, timeLeftOnTimer)
+        countDownTimer.cancel()
+
+        Log.d(TAG, "onSaveInstanceState: Saving score : $score & time $timeLeftOnTimer")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "ondestroy")
     }
 
     private fun resetGame(){
@@ -38,6 +90,7 @@ class MainActivity : AppCompatActivity() {
 
         countDownTimer = object: CountDownTimer(initialCountDown, countDownInterval){
             override fun onTick(millisUntilFinished: Long) {
+                timeLeftOnTimer = millisUntilFinished
                 val timeLeft = millisUntilFinished / 1000
                 timeLeftTextView.text = getString(R.string.time_left, timeLeft)
             }
